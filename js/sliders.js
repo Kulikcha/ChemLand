61aa0053e60be97e0e604f4ea6057093cf017e19
Kulikcha <57537491+Kulikcha@users.noreply.github.com>
2019-12-28 02:23:14 +0300

    Add files via upload

---
+++ js/sliders.js
@@ -0,0 +1,27 @@
+
+/*$('.examplesblock__works').slick({prevArrow:$('.leftarrow'),
+                                 nextArrow:$('.rightarrow'),
+								 slidesToShow:12,
+								 infinite:true,
+								 rows:3
+								 });*/
+								 
+/*var myCarousel = $('.examplesblock__works').slick({
+  prevArrow:$('.leftarrow')
+  nextArrow:$('.rightarrow'),
+  slidesToShow:12,
+  infinite:true,
+  slidesPerRow: 4, 
+  rows: 3
+});
+
+myCarousel.slick( 'slickSetOption', {
+  slidesPerRow: 4, 
+  rows: 3
+}, true );*/
+
+$(document).ready(function() {
+    $('.commentsblock__slider').slick({dots:true, 
+	                                   
+									   });
+});
